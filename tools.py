acfe11b70cb678848e2795f6ff11519c1b12b198
eedk <kde9604@naver.com>
2023-03-07 16:31:22 +0900

    custom matplot

--- tools.py
+++ tools.py
@@ -6,7 +6,16 @@ def changeFileName(path, targetName):
     i = 1
     for name in fileNames:
         src = os.path.join(path, name)
-        dst = targetName + str(i) + '.jpg'
+        dst = targetName + '.' + str(i) + '.jpg'
         dst = os.path.join(path, dst)
         os.rename(src, dst)
         i += 1
+
+
+dataDir = './dataset/AI-DATASET/data'
+train_dir = os.path.join(dataDir, 'trainData')
+
+
+examDir = os.path.join(train_dir, 'snack')
+
+changeFileName(examDir, 'snack')
