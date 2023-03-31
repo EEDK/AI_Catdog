5f9085667e0472607172ae4a28b0d9c1d5e3fef1
eedk <kde9604@naver.com>
2023-03-31 16:23:43 +0900

    set two-model bottle and can

--- tools.py
+++ tools.py
@@ -1,21 +1,18 @@
 import os
 
+class tools:
+    def changeFileName(path, targetName):
+        fileNames = os.listdir(path)
+        i = 1
+        for name in fileNames:
+            src = os.path.join(path, name)
+            dst = targetName + '.' + str(i) + '.jpg'
+            dst = os.path.join(path, dst)
+            os.rename(src, dst)
+            i += 1
 
-def changeFileName(path, targetName):
-    fileNames = os.listdir(path)
-    i = 1
-    for name in fileNames:
-        src = os.path.join(path, name)
-        dst = targetName + '.' + str(i) + '.jpg'
-        dst = os.path.join(path, dst)
-        os.rename(src, dst)
-        i += 1
+# dataDir = './dataset/AI-DATASET/data'
+# train_dir = os.path.join(dataDir, 'trainData')
+# examDir = os.path.join(train_dir, 'beverage_can')
 
-
-dataDir = './dataset/AI-DATASET/data'
-train_dir = os.path.join(dataDir, 'trainData')
-
-
-examDir = os.path.join(train_dir, 'snack')
-
-changeFileName(examDir, 'snack')
+# tools.changeFileName(examDir, 'beverage_can')
